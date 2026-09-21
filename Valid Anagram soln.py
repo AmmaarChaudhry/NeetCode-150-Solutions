28bf8d8a01ae04c6703a270f677122c9225cb58c
Ammaar <ammaar.ch@outlook.com>
2026-09-21 20:45:25 +0100

    Valid Anagram Efficient Soln

--- Valid Anagram soln.py
+++ Valid Anagram soln.py
@@ -1,4 +1,4 @@
-def isAnagram(s: str, t: str) -> bool:
+def oldisAnagram(s: str, t: str) -> bool:
     #Inefficient solution:
     if len(s) != len(t):
         return False
@@ -19,8 +19,43 @@ def isAnagram(s: str, t: str) -> bool:
         else:
             return False
     return True
+
+def isAnagram(s: str, t: str) -> bool:
+    #better method, puts all chars from s and t strings into dictionaries and compares them
+    if len(s) != len(t):
+        return False
+    
+    s_dict = {}
+    t_dict = {}
+    
+    for char in s:
+        if char in s_dict.keys():
+            s_dict[char] += 1
+        else:
+            s_dict.setdefault(char,1)
+            
+    for char in t:
+        if char in t_dict.keys():
+            t_dict[char] += 1
+        else:
+            t_dict.setdefault(char,1)
+            
+    if s_dict == t_dict:
+        return True
+    else:
+        return False
+            
+    print(s_dict)
+    print(t_dict)
+    
+    return True
+            
+    
+    
     
     
 print(isAnagram("Hello","Hello"))
+
+
 
         
