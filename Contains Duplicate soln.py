e9d2434f1775c6b3b4b744c83dca09d24923b0a9
Ammaar <ammaar.ch@outlook.com>
2026-09-21 19:50:00 +0100

    Completed Contains Duplicate

--- Contains Duplicate soln.py
+++ Contains Duplicate soln.py
@@ -1,5 +1,5 @@
 #Inefficient method:
-def hasDuplicate(nums: List[int]) -> bool:
+def hasDuplicateOld(nums: List[int]) -> bool:
     check_list = []
     for num in nums:
         if len(check_list) != 0:
@@ -9,6 +9,13 @@ def hasDuplicate(nums: List[int]) -> bool:
         check_list.append(num)
     return False
 
+def hasDuplicate(nums: List[int]) -> bool:
+    #Much more efficient method, compares nums list to nums set. As set can't contain duplicates,
+    #this allows us to verify if nums list contains duplicates.
+    #This passes NeetCode verification!
+    if len(nums) == len(set(nums)):
+        return False
+    return True
 
 
 nums = [1,2,3,1]
