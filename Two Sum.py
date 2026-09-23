ee089723768c8cbf3b06008784f3eadc44f5e5b7
Ammaar <ammaar.ch@outlook.com>
2026-09-23 17:06:52 +0100

    Finished two sum

--- Two Sum.py
+++ Two Sum.py
@@ -8,10 +8,11 @@ def twoSum(nums: List[int], target: int) -> List[int]:
     for j in range(0, len(nums)):
         if nums[j] in dictionary:
             return_i = dictionary.get(nums[j])
-            if(return_i<j):
-                return[return_i,j]
-            else:
-                return[j,return_i]
+            if (return_i != j):     
+                if(return_i<j):
+                    return[return_i,j]
+                else:
+                    return[j,return_i]
     
     
         
@@ -21,20 +22,9 @@ target = 7
 
 print(twoSum(nums,target))
 
-class Solution:
-    def twoSum(nums: List[int], target: int) -> List[int]:
-        dictionary = {}
-        for i in range(0, len(nums)):
-            difference = target - nums[i]
-            dictionary[difference] = i
-        print(dictionary)
-        
-        for j in range(0, len(nums)):
-            if nums[j] in dictionary:
-                return_i = dictionary.get(nums[j])
-                if(return_i<j):
-                    return[return_i,j]
-                else:
-                    return[j,return_i]
+
+    
+
+
 
         
