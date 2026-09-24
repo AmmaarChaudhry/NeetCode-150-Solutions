f85c21c06d6f3e5cc564a3e5061fb78d6c9d4497
Ammaar <ammaar.ch@outlook.com>
2026-09-24 12:36:02 +0100

    Better Group Anagrams soln

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -5,8 +5,17 @@ find sets for these char lists
 Create Hash Map, with the sets as a key, and associated lists of chars as values
 Rebuild list of lists
 
+edit: This doesnt work! You can't have a set as a key as it is unhashable.
+Also you don't need to break up strings into char lists. You can convert straight
+to set.
+Also why are you even using sets? You need to care about char freq as you are finding anagrams
+New plan:
+CCreate a hash map w/ sorted words as keys and anagrams from the list as values
+then you can build a list of collected values
+
+
 """
-def groupAnagrams(strs: List[str]) -> List[List[str]]:
+def groupAnagramsOld(strs: List[str]) -> List[List[str]]:
     dicts = {}
     for word in strs:
         char_list = []
@@ -22,6 +31,22 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
         
         
     return True
+    
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    dicts = {}
+    for word in strs:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in dicts:
+            #dicts[sorted_word] +=1
+            dicts[sorted_word].append(word)
+        else:
+            #dicts.setdefault(sorted_word,1)
+            dicts.setdefault(sorted_word,[word])
+
+    sorted_anagrams = dicts.values()
+    return list(sorted_anagrams)
+
+        
     
     
     
